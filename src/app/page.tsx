69a6403608827c6ce8dfea34174f7344be1047f2
Julian Castano <julian@castano.app>
2025-03-24 05:20:42 -0500

    fix: fix topics url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { App } from "@/components/App";
 
 export default async function Home() {
-  const topics = await fetch(process.env.SDK_BASE_URL!, {
+  const topics = await fetch(`${process.env.SDK_BASE_URL!}/topics`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + process.env.SDK_TOKEN!,
