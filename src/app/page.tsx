15f4aed77a2c3f34abdf310a050731b21fa9df9a
Julian Castano <julian@castano.app>
2025-03-24 05:16:23 -0500

    fix: fix another instance of api token

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { App } from "@/components/App";
 
 export default async function Home() {
-  const topics = await fetch("http://localhost:3000/topics", {
+  const topics = await fetch(process.env.SDK_BASE_URL!, {
     headers: {
       "Content-Type": "application/json",
-      Authorization:
-        "Bearer " +
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXJuYW1lIjoiVGhlQ2hlc3NEZXYiLCJpYXQiOjE3NDI3OTY1NjUsImV4cCI6MTc0MzY5NjU2NX0.-Q-g4OEubCxLSdqwmHNOvQ7hQky1Kk30O5J35DZTJyU",
+      Authorization: "Bearer " + process.env.SDK_TOKEN!,
     },
   }).then((res) => res.json());
 
